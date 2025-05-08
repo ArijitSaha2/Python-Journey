bd71ff2a61d4be6069266a8f58167fbad60c0a9d
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-08 20:56:57 +0530

    Added Chapter 0 : what is programming?

---
+++ Chapter 0/first.py
@@ -0,0 +1,23 @@
+print("Hello Za Warudo")
+
+
+# WHAT IS PROGRAMMING?
+# Just like we use Hindi or English to communicate with each other.
+# we use a programming language like Python to communicate with the computer.
+
+# Programming is a way to instruct the computer to perform various tasks.
+
+# WHAT IS PYTHON?
+# Python is a simple and easy to understand language which feels like reading simple English. 
+# This Pseudo code nature is easy to learn and understandable by beginners.
+
+# FEATURES OF PYTHON:
+# 1]Easy to understand=Less development time
+# 2]Free and open source
+# 3]High Level language
+# 4]Portable: Works on Linux / Windows / Mac.
+# 5]Fun to work with!
+
+# INSTALLATION:
+# Python can be easily installed from python.org. When you click on the download button.
+# python can be installed right after you complete the setup by executing the file for your platform.
