869601e1b7f7c1aa610dba6ee9147f1486168b90
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-09 21:04:57 +0530

    Chapter 7: While loop with explanation

---
+++ Chapter 7/02_while_loop.py
@@ -0,0 +1,17 @@
+# WHILE LOOP:
+# In while loops, the condition is checked first.
+# If the condition is true, the loop body executes.
+# The process of checking the condition and executing the body repeats until the condition is false
+i = 1
+while(i<6):# The block keeps executing until the condition is true
+    print(i)
+    i += 1
+
+"""
+Output:
+1
+2
+3
+4
+5
+"""
