632b4224589daf961728e002b16be8e61d09b96a
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-09 21:02:14 +0530

    Added Chapter 7: Loops in Python

---
+++ Chapter 7/01_loops.py
@@ -0,0 +1,23 @@
+# Loops in Python
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+# The same task can be done like this:
+
+
+for i in range(1, 20):
+    print(i)
+'''
+Sometimes we want to repeat a set of statements in our program.  For instance: Print 1 to 1000.
+
+Loops make it easy for a programmer to tell the computer which set of instructions to repeat and how!
+
+Types of Loops in Python :
+Primarily there are two types of loops in Python:
+while loops
+for loops
+'''
