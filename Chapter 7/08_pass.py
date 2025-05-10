19635fee052fde7ba58ffce38a9f4e586a877edc
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-10 12:37:31 +0530

    Chapter 7: Pass statement

---
+++ Chapter 7/08_pass.py
@@ -0,0 +1,14 @@
+# PASS STATEMENT
+
+"""
+pass is a null statement in python.
+It tells the interpreter to do nothing.
+"""
+
+for i in range(40):
+ pass # Without pass, an empty loop block will raise an IndentationError
+
+i = 0
+while i < 100:
+    print(i)
+    i += 1
