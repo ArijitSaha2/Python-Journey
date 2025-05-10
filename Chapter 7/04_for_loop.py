fec934e867da0a5f0a25ceeddc8ce10cf90da47b
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-10 11:32:32 +0530

    Chapter 7:  For loop with explanation

---
+++ Chapter 7/04_for_loop.py
@@ -0,0 +1,16 @@
+# FOR :
+# A for loop is used to iterate through a sequence like list, tuple, or string (iterables).
+    
+for i in range(4): # AN EXAMPLE DEMONSTRATING RANGE() FUNCTION.
+    print(i)
+"""
+RANGE FUNCTION IN PYTHON :
+
+The range() function in Python is used to generate a sequence of numbers.
+
+We can also specify the start, stop, and step size as follows:
+
+      range(start, stop, step_size)
+
+step_size is usually not used with range()
+"""
