97e3fc07d0fa8e2a7b47d7bfa9e218101bc0808c
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-10 11:59:33 +0530

    Chapter 7: List using for loop

---
+++ Chapter 7/05_list_using_for.py
@@ -0,0 +1,13 @@
+L = [1,2,3,4,False,True,"Harry"] # List Example with for loop
+for i in L:
+    print(i)
+
+
+t = (1,2,22,33,11,1000) # Tuple Example with for loop
+for i in t:
+    print(i)
+
+
+s = "Harry" # String Example with for loop
+for i in s:
+    print(i) 
