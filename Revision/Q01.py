f348c889d042c29c056c458e789dd721ced86a6d
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-14 20:21:53 +0530

    Q01 Revision

---
+++ Revision/Q01.py
@@ -0,0 +1,20 @@
+# Define the difference between a for loop and a while loop in Python. When would you prefer one over the other?
+
+''' for loop:
+Used when the number of iterations is known or predefined. It iterates over a sequence (like a list, tuple, or range).
+for i in range(start, stop, step):
+    # code block
+
+for i in range(1, 10, 2):
+print(i)  # Outputs 1, 3, 5, 7, 9
+'''
+
+''' while loop:
+Used when the number of iterations is unknown. It continues to execute as long as the condition remains true.
+syntax: while condition:
+    # code block
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+'''
