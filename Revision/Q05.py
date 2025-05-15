d00eec80d06d596cc3317d77a08c869a01f9d10a
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-15 18:40:46 +0530

    Q05 Revision

---
+++ Revision/Q05.py
@@ -0,0 +1,5 @@
+# Write a program that asks the user for their name and then prints a simple greeting message including their name.
+
+name = input("Enter youur name: ")
+
+print(f"Hola, {name}!!") 
