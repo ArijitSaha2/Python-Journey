e93608cc06d124d8fc11d43ae6d4b832e870f1d5
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-17 20:40:40 +0530

    ADDED Problem05

---
+++ Practice Set Revision/Problem05.py
@@ -0,0 +1,9 @@
+# Voting Eligibility
+'Take age input, print if eligible to vote (age ≥ 18).'
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Eligible to Vote")
+else:
+    print("Not Eligible to Vote")
