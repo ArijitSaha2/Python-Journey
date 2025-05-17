8645c963f095f95a6b8a1104b118f49cbc68bb29
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-17 20:30:40 +0530

    ADDED Problem03

---
+++ Practice Set Revision/Problem03.py
@@ -0,0 +1,10 @@
+# String Slicing & f-strings
+"""Print first 3 characters of s = "Python" using slicing.
+Take user input for their name, print a welcome message with f-string."""
+
+s = "Python"
+print(s[0:3])
+
+name = input("Enter your Name: ")
+
+print(f"Welcome, {name}!")
