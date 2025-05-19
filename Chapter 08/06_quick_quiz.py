ae35f40f2739f01a737e438c3e54af5fb0d849fb
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-19 23:59:45 +0530

    Added Quick Quiz 2

---
+++ Chapter 08/06_quick_quiz.py
@@ -0,0 +1,7 @@
+# Write a function square_number(num) that takes a number as input and returns its square. Then call the function with a few sample numbers and print the results.
+
+def square_number(number):
+    return number ** 2
+
+num = int(input("Enter your Number: "))
+print(square_number(num))
