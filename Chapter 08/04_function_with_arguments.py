c672592a9afccf4252750e4c09e4970f71755bb8
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-19 23:21:44 +0530

    Added Functions with Arguments

---
+++ Chapter 08/04_function_with_arguments.py
@@ -0,0 +1,18 @@
+# def goodday(name, ending):
+#     print("Good Day, " + name)
+#     print(ending)
+
+# goodday("Harry", "Bye Bye") 
+
+# def add(x, y):  # x and y are parameters (just like variables)
+#     print(x + y)
+
+# add(5, 3)       # 5 and 3 are arguments
+
+
+def greet(name):
+    gr = "hello " + name
+    return gr
+
+a = greet("harry")
+print(a)
