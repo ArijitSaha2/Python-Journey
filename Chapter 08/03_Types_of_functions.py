63695d7ffbf863af9c956d9eba9d0c6a30fca18d
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-19 12:55:17 +0530

    Added notes on Types of Functions (Built-in and User-defined)

---
+++ Chapter 08/03_Types_of_functions.py
@@ -0,0 +1,12 @@
+# TYPES OF FUNCTIONS IN PYTHON
+
+'''
+There are two types of functions in Python:
+
+1. Built-in Functions - Already present in Python.
+   Examples: len(), print(), range(), etc.
+
+2. User-defined Functions - Created by the user using the def keyword.
+   Example: def func1():  # custom logic here
+'''
+
