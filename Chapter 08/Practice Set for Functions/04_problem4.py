0087859a6c5d69ec6f238f3eca54646d0769935d
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-25 12:11:55 +0530

    add max_of_two function to compare two numbers

---
+++ Chapter 08/Practice Set for Functions/04_problem4.py
@@ -0,0 +1,10 @@
+# Function max_of_two(a, b) that returns the bigger number.
+
+def max_of_two(a,b):
+    if a > b:
+        print(f"{a} is bigger than {b}")
+    if b > a:
+        print(f"{b} is bigger than {a}")
+
+max_of_two(22,44)
+max_of_two(10,5)
