0d710a5fd4a6ddc8f719418489001b2eb608c43a
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-05-25 19:58:26 +0530

    add sum_three function to return sum of three numbers

---
+++ Chapter 08/Practice Set for Functions/06_problem6.py
@@ -0,0 +1,10 @@
+# Function sum_three(a, b, c) that returns the sum.
+
+def sum_three(a, b, c):
+    return a + b + c
+
+a = int(input('Enter number: '))
+b = int(input('Enter number: '))
+c = int(input('Enter number: '))
+
+print(sum_three(a, b, c))
