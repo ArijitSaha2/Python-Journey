938d4e2a6159ab667580638f794437f20c0522f0
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-06-02 23:11:29 +0530

    Added Problem30

---
+++ Practice Set Revision/Problem30.py
@@ -0,0 +1,29 @@
+# Chapter 3:
+# Write a program to check if a given number is even or odd using the modulus operator.
+
+def is_even(n):
+    if n < 0:
+        print("Positive Numbers only!!")
+    elif n % 2 == 0:
+        print("It is Even")
+    else:
+        print("It is Odd")
+n = int(input("Enter Number: "))
+is_even(n)
+
+
+# Chapter 4:
+# Write a program that asks the user for their age and prints whether they are a minor, adult, or senior citizen.
+
+def age(num):
+    if num < 1:
+        print("Invalid Age")
+    elif num < 18:
+        print("You're a Minor")
+    elif num >= 18 and num < 60:
+        print("You're an Adult")
+    elif num >= 60:
+        print("You're a Senior Citizen")
+
+num = int(input('Enter Age: '))
+age(num)
