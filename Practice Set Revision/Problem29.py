4608af0db3a23e27773d9ce14ccc2bf058e54fa4
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-06-02 23:08:24 +0530

    Added Problem29: Solutions from Chapter 1 and Chapter 2

---
+++ Practice Set Revision/Problem29.py
@@ -0,0 +1,18 @@
+# Chapter 1 (Basics & Syntax):
+# Write a Python program to print "Hello, World!" five times using a loop.
+
+n = 5
+
+for i in range(1, n+1):
+    print("Hello")
+
+# Chapter 2 (Variables & Data Types):
+# Create a program that takes two numbers as input and prints their sum, difference, product, and quotient.
+
+n1 = int(input('Enter Number1: '))
+n2 = int(input('Enter Number2: '))
+
+print(f'Sum = {n1 + n2}')
+print(f'Difference = {n1 - n2}')
+print(f'Product = {n1*n2}')
+print(f'Quotient = {n1//n2}')
