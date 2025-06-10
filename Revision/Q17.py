d8b2c23b2c0c4ead39ce079a0dc5622f11b4371e
ArijitSaha2 <Arijitsaha2334@gmail.com>
2025-06-10 21:08:46 +0530

    Q17 Revision

---
+++ Revision/Q17.py
@@ -0,0 +1,12 @@
+# Q: What is the difference between a variable and a keyword in Python?
+
+"""A variable is a name used to store data (like a container for values), and its contents can change during program execution.
+A keyword is a reserved word in Python that has a predefined meaning and purpose in the language (like if, else, while, def). You cannot use keywords as variable names. """
+
+# Q: What datatype does the input() function return? How do you convert it to an integer?
+
+"""The input() function returns a string by default.
+To convert it to an integer, wrap it with int(), like this:
+
+num = int(input("Enter a number: "))
+"""
